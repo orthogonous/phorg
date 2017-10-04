aa7a99fad18e805c04d19eb42527e91064ac4865
orthogonous <orthogonous@gmail.com>
2017-10-04 21:01:31 +1000

    Added better test for list_all

--- lib/phorg/flat_files.ex
+++ lib/phorg/flat_files.ex
@@ -18,23 +18,6 @@ defmodule Phorg.FlatFiles do
     Enum.map(skip_files_extensions, &(Regex.compile!("#{&1}$"))) |> 
     Enum.filter(&Regex.match?(&1, String.downcase(path))) |>
     _wanted_action()
-    #with [] do true
-
-
-    # Enum.map(&(Regex.compile!("#{&1}$"))) |>
-    
-    #Enum.map(fn e -> %{reg: Regex.compile!("#{e}$"), ext: e} end) |> 
-    #Enum.filter(&(String.match?(path, &1))) |>
-    #Enum.reduce(&(&1)) |>
-    #case length(&1) do
-    #  >0 -> false
-    #  _ -> true
-    #end
-      
-    #Enum.filter(fn %{reg: r, ext: e} -> String.match?(path, r) end) |>
-    # if path matches true to any regex filter based on true with Enum.filter
-    # |> Enum.reduce down to a number and if that number is greater than 0 then return false 
-
 
   end
 
@@ -45,9 +28,8 @@ defmodule Phorg.FlatFiles do
 
   defp _list_all(path) do
     cond do
-      String.contains?(path, ".mp4") -> []
-      true -> expand(File.ls(path), path)
-      false -> []
+      wanted_file(path) -> expand(File.ls(path), path)
+      true -> []
     end
   end
 
--- test/phorg/flat_files_test.exs
+++ test/phorg/flat_files_test.exs
@@ -9,12 +9,21 @@ defmodule Phorg.FlatFilesTest do
 
     files = FlatFiles.list_all(@test_photo_path)
 
-
     assert is_list(files) == true
     assert length(files) >= 8 # not the best test but make sure we have at least 8 items in here.
     assert "#{@test_photo_path}/1051.png" in files == true
     assert "#{@test_photo_path}/1052.png" in files == true
     assert "#{@test_photo_path}/1053.png" in files == true
+  end
+
+  test "list_all returns files in a directory but skipping unwanted files" do
+
+    files = FlatFiles.list_all(@test_photo_path)
+
+    refute "#{@test_photo_path}/bad.mov" in files == true
+    refute "#{@test_photo_path}/bad.mp4" in files == true
+    refute "#{@test_photo_path}/bad.mp3" in files == true
+    refute "#{@test_photo_path}/bad.mpeg" in files == true
   end
 
   test "wanted_file should return true for files we want" do
